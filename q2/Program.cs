352d11e89e7d5d0a0c54f3499a96f7f4f2dc9101
James Mc Guinness <S00236072@atu.ie>
2023-01-25 10:47:52 +0000

    q1(A)

--- q2/Program.cs
+++ q2/Program.cs
@@ -3,9 +3,25 @@
 {
     internal class Program
     {
+        /* Name: James McGuinness
+         * Date: 25/01/23
+         * Desc: Worksheet 1
+         * */
+
         static void Main(string[] args)
         {
-            Console.WriteLine("lets go again ");
+            PrintIntegersFor();
+            
+        }
+
+        static private void PrintIntegersFor()
+        {
+            for (int i = 40; i <= 60;)
+            {
+                Console.WriteLine($"{i}");
+                i = i + 1;
+                
+            } 
         }
     }   
 }
